7ba77737f00a3eaf9aedce17a2043e52029caad7
WFZ1 <pavelskumenko@gmail.com>
2024-08-18 10:44:35 +0300

    refactor: move coins api url to .env file

--- app/src/pages/сurrency.tsx
+++ app/src/pages/сurrency.tsx
@@ -50,7 +50,7 @@ const CurrencyPage = ({ serverData }: CurrencyPageProps) => {
 export const getServerData = async () => {
     try {
         const response = await fetch(
-            `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+            `${process.env.COINS_API_URL}/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`
         );
 
         if (!response.ok) {
